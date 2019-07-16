19f66d7998f1bc39e0c8c3cfba88d2dbd3cbfed4
Matt Schaeffer <matt.schaeffer@autodesk.com>
2019-07-16 16:37:56 -0600

    testing

--- v17_4_prerelease/location_formit.js
+++ v17_4_prerelease/location_formit.js
@@ -61,6 +61,7 @@ LocationDialog.FetchDashboardWidgets = function()
 {
     const test = FormIt.FetchDashboardWidgets();
 
+    console.log(test);
     debugger;
     return test;
     //return FormIt.FetchDashboardWidgets();
